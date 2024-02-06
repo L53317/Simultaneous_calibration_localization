5388a2742db3cefa2980e9547633cfba82019f2f
liu <liuxh.0@qq.com>
2024-02-06 16:31:53 +0100

    Fix a problem cause by UWBPositionMap[t] = tmp. Now the UWB position data are used to calculate the global position. However, the distance should not be added in the original problem because the distances are not a manifold directly. So adding the distance leding t the optimiazation result can not converge. Another method should be consider or different costfunction should be considered.

--- src/globalOpt.cpp
+++ src/globalOpt.cpp
@@ -76,7 +76,7 @@ void GlobalOptimization::inputUWB(double t, double x, double y, double z, double
 {
     vector<double> tmp{x, y, z, posAccuracy};
     //printf("new uwb: t: %f x: %f y: %f z:%f \n", t, tmp[0], tmp[1], tmp[2]);
-    // UWBPositionMap[t] = tmp; // For the same t, replace and update the data.
+    UWBPositionMap[t] = tmp; // For the same t, replace and update the data.
     newUWB = true;
 }
 
@@ -360,6 +360,88 @@ void GlobalOptimization::optimize()
             // printf("global time %f \n", globalOptimizationTime.toc());
             mPoseMap.unlock();
         }
+
+        /* update anchors' positions, with the optimization results. */
+        // if(newUWBdistance && (globalAnchorMap.size() > 0))
+        // {
+        //     newUWBdistance = false;
+
+        //     /* Create a new problem to be solved */
+        //     ceres::Problem problem;
+        //     ceres::Solver::Options options;
+        //     options.linear_solver_type = ceres::SPARSE_NORMAL_CHOLESKY;
+        //     options.max_num_iterations = 5;
+        //     ceres::Solver::Summary summary;
+        //     ceres::LossFunction *loss_function_anchors;
+        //     loss_function_anchors = new ceres::HuberLoss(1.0);
+
+        //     /* Add parameters for the optimization problem, the cost function */
+        //     mPoseMap.lock();
+        //     /* Add local poses in localPoseMap to factor graph */
+        //     int length = localPoseMap.size();
+        //     double p_array[length][3]; // UWB anchors' position variables to be estimated
+
+        //     int lengthAnchorMap = globalAnchorMap.size(); // TODO same with global pose sizes.
+        //     // UWBDistanceMap
+        //     map<double, vector<double>>::iterator iterAnchor;
+        //     iterAnchor = globalAnchorMap.begin();
+        //     for (int i = 0; iterAnchor != globalAnchorMap.end(); i++, iterAnchor++) // TODO
+        //     {
+        //         p_array[i][0] = iterAnchor->second[3];
+        //         p_array[i][1] = iterAnchor->second[4];
+        //         p_array[i][2] = iterAnchor->second[5];
+        //         problem.AddParameterBlock(p_array[i], 3);
+        //         // std::cout << p_array[i][0]<< ","<<p_array[i][1]<< ","<<p_array[i][2] << "\n";
+        //         // std::cout << iterAnchor->second[3]<< ","<<iterAnchor->second[4]<< ","<<iterAnchor->second[5] << "\n";
+        //     }
+
+        //     map<double, vector<double>>::iterator iterVIO, iterUWBrange; // , iterGlobal;
+        //     int i = 0;
+        //     for (iterVIO = localPoseMap.begin(); iterVIO != localPoseMap.end(); iterVIO++, i++)
+        //     // for (iterGlobal = globalPoseMap.begin(); iterGlobal != globalPoseMap.end(); iterGlobal++, i++)
+        //     {
+        //         double t = iterVIO->first;
+        //         map<double, vector<double>>::iterator iterGlobal, iterUWBAnchor;
+        //         iterGlobal = globalPoseMap.find(t);
+        //         iterUWBAnchor = globalAnchorMap.find(t);
+        //         // iterUWBrange = UWBDistanceMap.find(t); // Find UWB poses with the same t
+        //         // if (iterUWBrange != UWBDistanceMap.end())
+        //         // TODO: globalPoseMap may not have same length and may not with same position.
+        //         if (iterUWBAnchor != globalAnchorMap.end() && iterGlobal != globalPoseMap.end())
+        //         {   /* UWB cost functions, and the mearsurements . */
+        //             ceres::CostFunction* uwbrange_function = DError::Create(
+        //                 // DError::Create(iterUWBrange->second[3], iterUWBrange->second[4]);
+        //                 // iterVIO->second[0], iterVIO->second[1], iterVIO->second[2], iterUWBrange->second[3], iterUWBrange->second[4]);
+        //                 iterGlobal->second[0], iterGlobal->second[1], iterGlobal->second[2],
+        //                 // iterUWBrange->second[3], iterUWBrange->second[4]);
+        //                 iterUWBAnchor->second[6], iterUWBAnchor->second[7]);
+        //                 // TODO: Add constant condiction of anchor position ! ******** ! //
+        //             problem.AddResidualBlock(uwbrange_function, loss_function_anchors, p_array[i]);
+        //             // double tmp[3] = {p_array[i][0],p_array[i][1],p_array[i][2]};
+        //             // problem.AddResidualBlock(uwbrange_function, loss_function_anchors, tmp);
+        //         }
+        //     }
+
+        //     // mPoseMap.unlock();
+        //     ceres::Solve(options, &problem, &summary);
+        //     // std::cout << summary.BriefReport() << "\n";
+
+        //     printf("UWB anchor optimization: %s(%d): \n" ,__FILE__, __LINE__);
+        //     /* update anchors' positions, with the optimization results. */
+
+        //     map<double, vector<double>>::iterator iterUWBAnchorMap;
+        //     iterUWBAnchorMap = globalAnchorMap.begin();
+        //     // for (int i = 0; i < length; i++, iter++)
+        //     for (int i = 0; iterUWBAnchorMap != globalAnchorMap.end(); i++, iterUWBAnchorMap++)
+        //     {
+        //         vector<double> uwbAnchorPosition{iterUWBAnchorMap->second[0], iterUWBAnchorMap->second[1], iterUWBAnchorMap->second[2],
+        //                                             p_array[i][0], p_array[i][1], p_array[i][2]};
+        //         iterUWBAnchorMap->second = uwbAnchorPosition; // save the solved global poses
+        //     }
+        //     updateAnchorMap();
+
+        //     mPoseMap.unlock();
+        // }
 
         std::chrono::milliseconds dura(2000); // Update rate for optimization calculation (can be smaller)
         std::this_thread::sleep_for(dura);
